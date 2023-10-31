eaaeb4402b23a3cbfa95182b15d4e3deba93eaa9
Vincent Hou <shou73@bloomberg.net>
2023-10-31 12:01:02 -0400

    Add the e2e test cases

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -17,6 +17,46 @@
 source $(dirname $0)/e2e-common.sh
 
 # Script entry point.
-initialize $@  --skip-istio-addon
+initialize --num-nodes=4 --enable-ha --cluster-version=1.26 "$@"
 
+# Run the tests
+header "Running tests"
+
+failed=0
+
+# Run tests serially in the mesh and https scenarios.
+GO_TEST_FLAGS=""
+E2E_TEST_FLAGS="${TEST_OPTIONS}"
+
+if [ -z "${E2E_TEST_FLAGS}" ]; then
+  E2E_TEST_FLAGS="-resolvabledomain=$(use_resolvable_domain) -ingress-class=${INGRESS_CLASS}"
+
+  # Drop testing alpha and beta features with the Gateway API
+  if [[ "${INGRESS_CLASS}" != *"gateway-api"* ]]; then
+    E2E_TEST_FLAGS+=" -enable-alpha -enable-beta"
+  fi
+fi
+
+if (( HTTPS )); then
+  E2E_TEST_FLAGS+=" -https"
+  toggle_feature external-domain-tls Enabled config-network
+  kubectl apply -f "${E2E_YAML_DIR}"/test/config/externaldomaintls/certmanager/caissuer/
+  add_trap "kubectl delete -f ${E2E_YAML_DIR}/test/config/externaldomaintls/certmanager/caissuer/ --ignore-not-found" SIGKILL SIGTERM SIGQUIT
+fi
+
+if (( MESH )); then
+  GO_TEST_FLAGS+=" -parallel 1"
+fi
+
+if (( SHORT )); then
+  GO_TEST_FLAGS+=" -short"
+fi
+
+cd ${KNATIVE_DIR}/"serving"
+go_test_e2e -timeout=30m \
+  ${GO_TEST_FLAGS} \
+  ./test/e2e \
+  ${E2E_TEST_FLAGS} || failed=1
+
+(( failed )) && fail_test
 success

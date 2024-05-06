a4beb7050ea2cf21fd1595b88ccf16ac5179669b
Vincent Hou <shou73@bloomberg.net>
2024-05-06 14:40:30 -0400

    Rename the rollout strategies into availability and resourceUtil

--- pkg/reconciler/rolloutorchestrator/rolloutmodes/base.go
+++ pkg/reconciler/rolloutorchestrator/rolloutmodes/base.go
@@ -34,8 +34,12 @@ import (
 )
 
 var (
-	NormalMode      = "normal"
-	MaintenanceMode = "maintenance"
+	// AvailabilityMode is one strategy to roll out the new revision of the knative service, making sure the service is
+	// always available.
+	AvailabilityMode = "availability"
+	// ResourceUtilMode is one strategy to roll out the new revision of the knative service, making sure the resource
+	// is used optimized. It is possible that the service can have the downtime.
+	ResourceUtilMode = "resourceutil"
 )
 
 // The RolloutStep interface defines all the functions, that are necessary to call to accomplish the rollout step.

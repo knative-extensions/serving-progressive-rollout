981221443f0de3d0333387516ea1dd17224456df
Vincent Hou <shou73@bloomberg.net>
2024-10-11 13:54:29 -0400

    Migrate to use kube codegen in update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,6 +19,8 @@ set -o nounset
 set -o pipefail
 
 source $(dirname $0)/../vendor/knative.dev/hack/codegen-library.sh
+source "${CODEGEN_PKG}/kube_codegen.sh"
+
 export PATH="$GOBIN:$PATH"
 
 function run_yq() {
@@ -29,14 +31,12 @@ echo "=== Update Codegen for ${MODULE_NAME}"
 
 group "Kubernetes Codegen"
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
-  knative.dev/serving-progressive-rollout/pkg/client knative.dev/serving-progressive-rollout/pkg/apis \
-  "serving:v1" \
-  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
+kube::codegen::gen_client \
+  --boilerplate "${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt" \
+  --output-dir "${REPO_ROOT_DIR}/pkg/client" \
+  --output-pkg "knative.dev/serving-progressive-rollout/pkg/client" \
+  --with-watch \
+  "${REPO_ROOT_DIR}/pkg/apis"
 
 group "Knative Codegen"
 
@@ -45,6 +45,12 @@ ${KNATIVE_CODEGEN_PKG}/hack/generate-knative.sh "injection" \
   knative.dev/serving-progressive-rollout/pkg/client knative.dev/serving-progressive-rollout/pkg/apis \
   "serving:v1" \
   --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt
+
+group "Deepcopy Gen"
+
+kube::codegen::gen_helpers \
+  --boilerplate "${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt" \
+  "${REPO_ROOT_DIR}/pkg/apis"
 
 group "Update deps post-codegen"
 

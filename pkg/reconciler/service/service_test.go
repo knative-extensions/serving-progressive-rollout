dedecda4ecd3f60f1ee3105b336dd713874d00ee
Vincent <shou73@bloomberg.net>
2025-05-06 05:12:58 -0400

    [release-1.16] Assign 0% when there is at least one pod running (#356)

--- pkg/reconciler/service/service_test.go
+++ pkg/reconciler/service/service_test.go
@@ -1020,6 +1020,11 @@ func TestTransformService(t *testing.T) {
 							LatestRevision: ptr.Bool(false),
 							Percent:        ptr.Int64(100),
 						},
+						{
+							ConfigurationName: "test-name",
+							Percent:           ptr.Int64(0),
+							LatestRevision:    ptr.Bool(true),
+						},
 					},
 				},
 			},

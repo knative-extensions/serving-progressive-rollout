9fe4d50cee59df3a5cf3aec8c044e2bf0238650d
David Simansky <dsimansk@redhat.com>
2025-06-17 23:23:57 +0200

    Update k8s deps version aligned to knative/pkg (#375)
    * Update k8s deps version aligned to knative/pkg
    
    * Fix linter issues
    
    * Fix expected error msg in unit tests
    
    * Bump kind setup to latest tag

--- pkg/client/informers/externalversions/serving/v1/stagepodautoscaler.go
+++ pkg/client/informers/externalversions/serving/v1/stagepodautoscaler.go
@@ -62,13 +62,25 @@ func NewFilteredStagePodAutoscalerInformer(client versioned.Interface, namespace
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ServingV1().StagePodAutoscalers(namespace).List(context.TODO(), options)
+				return client.ServingV1().StagePodAutoscalers(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.ServingV1().StagePodAutoscalers(namespace).Watch(context.TODO(), options)
+				return client.ServingV1().StagePodAutoscalers(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options metav1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ServingV1().StagePodAutoscalers(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options metav1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.ServingV1().StagePodAutoscalers(namespace).Watch(ctx, options)
 			},
 		},
 		&apisservingv1.StagePodAutoscaler{},
